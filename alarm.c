7ac8c284162c28c6515037b3507723b298e14747
sonu99 <sks993463@gmail.com>
2016-02-06 23:50:35 +0530

    Program for demonstration of signal and alarm..it will kill the process after 6 sec

---
+++ alarm.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<signal.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<wait.h>
+void sig_hand(int sig_no)
+	{
+		printf("alaram is raised and signal no=%d\n",sig_no);
+		kill(getpid(),SIGINT);
+		printf("process is killed after 6 sec\n");	
+	}
+
+void main()
+	{
+		printf("process id=%d\n",getpid());
+		signal(SIGALRM,sig_hand);
+		alarm(6);
+		while(1);
+	}
