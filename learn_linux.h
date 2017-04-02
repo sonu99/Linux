9351d4cd7e28f69b2702bbf283eff232496e6075
sonu99 <sonu99@users.noreply.github.com>
2017-04-02 10:50:58 +0530

    Create learn_linux.h

---
+++ learn_linux.h
@@ -0,0 +1,21 @@
+1> start with notes.
+Q>---> what is os.
+Ans: 1> os will allocate the resource in efficient manaer, so it will helps to do multi tasking, multi processing,
+     2> os will security to process
+           -> each process will run at seprate virtual space.
+           -> and other process can't touch it. for doing that we can use IPC.
+           
+Q> multi tasking, multiprocessing, multi thereading.
+-> multi tasking:- ablity to run more than one task. (notpad and firefox)
+-> multi processing: - ablity of os to run process, on more than one core or handle or controll more than one than.
+-> multi threading:-    
+Q> ---> diffrent between linux os and RTOS
+     -> task should done at fixed time...(quality at fixed time)
+     -> (focus on multitasking)
+Q---> How we can make RTOS and RT linux
+Ans--?
+---> what is user sapce.
+Ans:-
+---> what is kernel space 
+---> why os divided in user and kernel space.
+------------------------------------------------------------------Process manager--------------------------------------------
