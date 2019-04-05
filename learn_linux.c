6f97a293e1c2c03e9fadb6cf0efda205e2fb4e32
sonu99 <sonu99@users.noreply.github.com>
2019-04-05 13:47:54 +0530

    Update learn_linux.c

--- learn_linux.c
+++ learn_linux.c
@@ -1,3 +1,13 @@
 memory managment:
 http://www.tldp.org/LDP/tlk/mm/memory.html
 http://tldp.org/LDP/lpg/
+
+
+
+Locking machanism:
+http://www.c-program-example.com/2012/04/c-program-to-lock-file-using-semaphores.html 
+
+http://www.sanfoundry.com/linux-debugging-questions-answers-posix-ipcs/
+
+
+
